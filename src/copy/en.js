e883406e4011f8253f8cf86d2900fce1144cdf66
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-07 14:52:25 +0000

    rm internal sheet id from copy error

--- src/copy/en.js
+++ src/copy/en.js
@@ -4,7 +4,7 @@ export default {
     onlySheet: 'You cannot query a sheet directly. The URL needs to be in the format /:sheet/:tab/:resource.',
     onlyTab: 'You cannot query a tab directly. The URL needs to be in the format /:sheet/:tab/:resource.',
     noSheet: sheet => `The sheet ${sheet} is not available in this server.`,
-    noResource: prts => `The resource '${prts[2]}' does not exists in the tab '${prts[1]}' of the sheet '${prts[0]}'.`,
+    noResource: prts => `The resource '${prts[2]}' does not exists in the tab '${prts[1]}' in this sheet.`,
     noFragment: prts => `Fragment index does not exist`
   },
   success: {
