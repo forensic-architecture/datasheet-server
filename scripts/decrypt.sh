3388d18eb32a49df6920c6a53282ba2457d1f930
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-14 18:43:55 +0000

    fix travis decrypt logic

--- scripts/decrypt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openssl aes-256-cbc -K $encrypted_4f23b46f1ec6_key -iv $encrypted_4f23b46f1ec6_iv -in .env.enc -out .env -d
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-travis encrypt-file .env --force
+travis encrypt-file .env --add --force
 git add .env.enc
 git commit -m "TRAVIS: add encrypted key"
