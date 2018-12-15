4328ceb464710cbd67355caffbaa48d6c2be289c
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-15 10:59:20 +0000

    encrypt script for travis

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-travis encrypt-file .env --add --force
-git add .env.enc
-git commit -m "TRAVIS: add encrypted key"
+echo "Encrypting .env file for Travis..."
+if hash travis 2>/dev/null; then
+	if [ ! -f .env ]; then
+		echo "============================================================================================"
+		echo "ERROR: You must create a .env file and add your credentials. See .env.example for an example"
+		echo "============================================================================================"
+		exit 3
+	else
+		travis encrypt-file .env --add --force --pro
+		git add .env.enc
+		git add .travis.yml
+		echo ".env.enc created and added to commit"
+	fi
+else
+	echo "============================================================================================"
+	echo "ERROR: Travis CLI is not installed on your local. Please install from:"
+	echo "\thttps://github.com/travis-ci/travis.rb"
+	echo "After installing, make sure that you login with:"
+	echo "\ttravis login --pro"
+	echo "============================================================================================"
+	exit 3
+fi
