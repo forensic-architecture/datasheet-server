b6d917553c1e26d0ad108f9863505cab7bd57bb3
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-29 13:28:11 +0100

    add deepids as default for sources

---
+++ src/blueprinters/deepids.js
@@ -0,0 +1,24 @@
+import { fmtObj } from '../lib/util'
+import deeprows from './deeprows'
+
+/**
+ * Each resource item is an object with values labelled according
+ * to column names specified in the sheet's first row. If two or more
+ * column names are the same except for a different integer at the end
+ * (e.g. 'tag1', and 'tag2'), then the values of those two columns are
+ * aggregated into a list, which is the value of the prefix's key ('tag').
+ * Any values in those columns that are empty will NOT be added to the list.
+ *
+ * @param  {type} data        list of lists representing sheet data.
+ * @return {type} Object      the structured data.
+ */
+export default (data) => {
+  const output = {}
+
+  deeprows(data).forEach(row => {
+    output[row.id] = row
+  })
+
+  return output
+}
+
--- src/sheets_config.js
+++ src/sheets_config.js
@@ -9,7 +9,20 @@ export default {
         tabs: {
           export_events: [BP.deeprows, BP.rows],
           export_categories: [BP.groups, BP.rows],
-          export_sources: BP.ids,
+          export_narratives: BP.rows,
+          export_sources: BP.deepids,
+          export_sites: BP.rows,
+          export_tags: BP.tree
+        }
+      },
+      {
+        name: 'ilovaisk',
+        id: '1fmK02NZSYM4gJL1KiSgjkIeo90NwY58o2QNYUepu1Fw',
+        tabs: {
+          export_events: [BP.deeprows, BP.rows],
+          export_categories: [BP.groups, BP.rows],
+          export_narratives: BP.rows,
+          export_sources: BP.deepids,
           export_sites: BP.rows,
           export_tags: BP.tree
         }
