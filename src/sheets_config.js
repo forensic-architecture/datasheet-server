a124d2c9bfb663aed593d453676de35d09da466e
Lachlan Kermode <lachiekermode@gmail.com>
2019-01-04 12:16:58 +0000

    deep regex non-greedy

--- src/sheets_config.js
+++ src/sheets_config.js
@@ -14,18 +14,6 @@ export default {
           export_sites: BP.rows,
           export_tags: BP.tree
         }
-      },
-      {
-        name: 'ilovaisk',
-        id: '1fmK02NZSYM4gJL1KiSgjkIeo90NwY58o2QNYUepu1Fw',
-        tabs: {
-          export_events: [BP.deeprows, BP.rows],
-          export_categories: [BP.groups, BP.rows],
-          export_narratives: BP.rows,
-          export_sources: BP.deepids,
-          export_sites: BP.rows,
-          export_tags: BP.tree
-        }
       }
     ]
   }
