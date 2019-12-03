b8de716397fed4156acaffa6294072d511483b36
Fraser Crichton <fraser.crichton.developer@gmail.com>
2019-12-04 10:25:16 +1300

    Update

--- src/sheets_config.js
+++ src/sheets_config.js
@@ -1,11 +1,17 @@
-import BP from './lib/blueprinters'
+import BP from "./lib/blueprinters";
+
+// state care
+// 1lc_jXGPfVxTi3dfJmFEVg-3xOuRIvdpZsbg6ki76_l8
+
+// Blair Peach
+// 1iPE3QiIdvUtimH8DlVae3GGUydgBQDTkoNlN5cfjK_o
 
 export default {
   googleSheets: {
     sheets: [
       {
-        name: 'example',
-        id: '1iPE3QiIdvUtimH8DlVae3GGUydgBQDTkoNlN5cfjK_o',
+        name: "example",
+        id: "1lc_jXGPfVxTi3dfJmFEVg-3xOuRIvdpZsbg6ki76_l8",
         tabs: {
           export_events: [BP.deeprows, BP.rows],
           export_categories: [BP.groups, BP.rows],
@@ -17,4 +23,4 @@ export default {
       }
     ]
   }
-}
+};
