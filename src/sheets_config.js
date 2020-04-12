f6c5b2e95089600cfcd319ff4e0e6ccdf28edcf3
Fraser Crichton <fraser.crichton.developer@gmail.com>
2020-04-12 12:16:24 +1200

    Merged with fork

--- src/sheets_config.js
+++ src/sheets_config.js
@@ -3,11 +3,6 @@ import BP from './lib/blueprinters'
 const timemapStructure = {
   teuruwera_export_events: BP.deeprows,
   teuruwera_export_categories: [BP.groups, BP.rows],
-  // Blair Peach
-  // 1iPE3QiIdvUtimH8DlVae3GGUydgBQDTkoNlN5cfjK_o
-
-  // FA
-  // 1ej3kEtANGYsEBUCTfWsIR0UhTSJIuXFHLYspti2sgHo
   export_narratives: BP.rows,
   teuruwera_export_sources: BP.deepids,
   export_sites: BP.rows,
