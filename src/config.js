fba74d8e9cf5cbd3054caf453194fdfadbc87ce3
efarooqui <efarooqui@pandora.com>
2020-08-11 21:40:34 -0700

    Wrote validation functions and getter to grab appropriate validation function

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 import { timemap } from './lib'
 
 export default {
-  gsheets: [],
+  gsheets: [
+    {
+      name: 'us2020',
+      id: '1I_pgyTQJlIorTIEHBxw1mM1STn-SrIi66FKYxut61iM',
+      tabs: timemap.default
+    }
+  ],
   xlsx: [
     {
       name: 'timemap_data',
