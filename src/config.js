972c5dc5db92969c8b0767671fd309261ea0caa4
Fraser Crichton <fraser.crichton.developer@gmail.com>
2021-02-10 09:28:18 +1300

    update

--- src/config.js
+++ src/config.js
@@ -1,23 +1,30 @@
-// import { timemap } from './lib'
-
-// export default {
-//   gsheets: [
-//     {
-//       name: 'curfew-2',
-//       id: '15dQTFRkcePCHF4Nfr_JhQWz1LfYtL8_pXhom2F4opWM',
-//       tabs: timemap.default
-//     }
-//   ],
-// }
 import { timemap } from './lib'
 
 export default {
-  gsheets: [],
-  xlsx: [
+  gsheets: [
     {
       name: 'timemap_data',
-      path: 'data/timemap_data.xlsx',
+      id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
       tabs: timemap.default
     }
-  ]
+  ],
 }
+// washington: 
+// id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
+
+//ACLED
+// id: '15dQTFRkcePCHF4Nfr_JhQWz1LfYtL8_pXhom2F4opWM',
+     
+
+// import { timemap } from './lib'
+
+// export default {
+//   gsheets: [],
+//   xlsx: [
+//     {
+//       name: 'timemap_data',
+//       path: 'data/timemap_data.xlsx',
+//       tabs: timemap.default
+//     }
+//   ]
+// }
